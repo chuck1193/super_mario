2c7739ec633e47546444870a62ce8df2d0029ef4
chuck1193 <>
2019-08-21 16:13:13 -0500

    Mario Jumpsgit add --all He really jumps. Refactored and added in a timer to make every time mario leaps, they are always the same.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 import Compositor from './compositor.js';
-import Entity from './entity.js';
+import Timer from './timer.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
 import {loadBackgroundSprites} from './sprites.js';
@@ -19,17 +19,21 @@ Promise.all([
   const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
   comp.layers.push(backgroundLayer);
 
-  const gravity = 0.5;
+  const gravity = 30;
+  mario.pos.set(64, 180);
+  mario.vel.set(200, -600);
+
 
   const spriteLayer = createSpriteLayer(mario);
   comp.layers.push(spriteLayer);
 
-function update(){
-  comp.draw(context);
-  mario.update();
-  mario.vel.y += gravity;
-  requestAnimationFrame(update);
+  const timer = new Timer(1/60);
+  timer.update = function update(deltaTime){
+    comp.draw(context);
+    mario.update(deltaTime);
+    mario.vel.y += gravity;
   }
-  update();
+
+  timer.start();
 
 });
